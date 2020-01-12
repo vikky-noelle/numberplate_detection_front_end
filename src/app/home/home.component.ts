42bd8ac12306e7cec45925a28a35a1c24da503cc
vikky-noelle <vikrant.attri2017@gmail.com>
2020-01-12 15:38:09 +0530

    new node modules

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, ViewChildren } from '@angular/core';
+import { QueryList } from '@angular/core/src/render3';
 
 @Component({
   selector: 'app-home',
@@ -8,8 +9,27 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
+  @ViewChild('target_one') target_one;
+  @ViewChildren('tiles') tiles: QueryList<any>;
+
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
+  }
+  scroll = (): void => {
+    // var element=document.getElementsByClassName("about");
+    console.log(window.pageYOffset);
+    if(window.pageYOffset > 200){
+      console.log(this.target_one);
+      // element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    }
+  else{
+  }
+};
+
+  scrollToElement($element): void {
+    console.log($element);  
+    $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 
 }
