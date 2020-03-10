09328bcc6bd66eeef5baf4f05592562794c1ed86
vikky-noelle <vikrant.attri2017@gmail.com>
2020-03-10 10:49:04 +0530

    major updates in UI, help added.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,26 +9,53 @@ import { QueryList } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   constructor() { }
-  @ViewChild('target_one', { static: true }) target_one;
   @ViewChildren('tiles') tiles: QueryList<any>;
 
+  isshowbot=false;
 
   ngOnInit() {
     window.addEventListener('scroll', this.scroll, true);
+    this.openbot();
+  }
+
+  openbot(){
+    console.log("openbot");
+    this.isshowbot=!this.isshowbot;
   }
   scroll = (): void => {
-    // var element=document.getElementsByClassName("about");
+    const arr = this.tiles.toArray();
     console.log(window.pageYOffset);
-    if(window.pageYOffset > 200){
-      console.log(this.target_one);
-      // element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    if(window.pageYOffset >=288&&window.pageYOffset <=1000){
+      console.log(arr[1].nativeElement.style);
+      arr[1].nativeElement.style.height="25px";
+      arr[1].nativeElement.style.width="25px";
+      arr[0].nativeElement.style.height="15px";
+      arr[0].nativeElement.style.width="15px";
+      arr[2].nativeElement.style.height="15px";
+      arr[2].nativeElement.style.width="15px";
     }
-  else{
-  }
+    else if(window.pageYOffset < 288){
+      console.log(arr[1].nativeElement.style);
+      arr[0].nativeElement.style.height="25px";
+      arr[0].nativeElement.style.width="25px";
+      arr[1].nativeElement.style.height="15px";
+      arr[1].nativeElement.style.width="15px";
+      arr[2].nativeElement.style.height="15px";
+      arr[2].nativeElement.style.width="15px";
+    }
+    else if(window.pageYOffset > 1000){
+      console.log("true");
+      arr[0].nativeElement.style.height="15px";
+      arr[0].nativeElement.style.width="15px";
+      arr[1].nativeElement.style.height="15px";
+      arr[1].nativeElement.style.width="15px";
+      arr[2].nativeElement.style.height="25px";
+      arr[2].nativeElement.style.width="25px";
+    }
+    
 };
 
   scrollToElement($element): void {
-    console.log($element);  
     $element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   }
 
