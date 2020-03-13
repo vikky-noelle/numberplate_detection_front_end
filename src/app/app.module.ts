3854bd12627c85697234caf7e2ebe21f54251eb0
vikky-noelle <vikrant.attri2017@gmail.com>
2020-03-13 16:21:44 +0530

    image upload functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+// location
+import { AgmCoreModule } from '@agm/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http'
@@ -14,7 +17,11 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyBhy2_i75_m05Chs918Q5UKG2fHxMlhia4'
+    }),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,20 +8,48 @@ import { QueryList } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  
+  imageurl: String = "assets/images/bot.png";
+  filetoupload: File = null;
   constructor(
     private http: HttpClient 
-  ) { }
+  ) {
+    if(navigator){
+      navigator.geolocation.getCurrentPosition(pos=>{
+        console.log("lala"+pos.coords.longitude);
+        console.log(+pos.coords.latitude);
+      });
+    }
+   }
   @ViewChildren('tiles') tiles: QueryList<any>;
 
   isshowbot=false;
 
   ngOnInit() {
-    this.http.get('http://127.0.0.1:8000/vehicle/?format=json').subscribe(res=>{
+    this.http.get('http://127.0.0.1:8100/vehicle/?format=json').subscribe(res=>{
       console.log(res);
     });
     window.addEventListener('scroll', this.scroll, true);
     this.openbot();
+  }
+  
+  changepic(file: FileList){
+    this.filetoupload=file.item(0);
+    // show image preview
+    var reader = new FileReader();
+    reader.onload = (event: any)=>{
+      this.imageurl=event.target.result;
+    }
+    reader.readAsDataURL(this.filetoupload);
+  }
+
+  onSubmit(){
+    let formdata = new FormData();
+    formdata.append('file', this.filetoupload);
+    console.log(this.filetoupload);
+    this.http.post('http://127.0.0.1:8100/vehicle/img',formdata).subscribe(res=>{
+      console.log("worked");
+    });
   }
 
   openbot(){
@@ -30,9 +58,9 @@ export class HomeComponent implements OnInit {
   }
   scroll = (): void => {
     const arr = this.tiles.toArray();
-    console.log(window.pageYOffset);
+    // console.log(window.pageYOffset);
     if(window.pageYOffset >=288&&window.pageYOffset <=1000){
-      console.log(arr[1].nativeElement.style);
+      // console.log(arr[1].nativeElement.style);
       arr[1].nativeElement.style.height="25px";
       arr[1].nativeElement.style.width="25px";
       arr[0].nativeElement.style.height="15px";
@@ -41,7 +69,7 @@ export class HomeComponent implements OnInit {
       arr[2].nativeElement.style.width="15px";
     }
     else if(window.pageYOffset < 288){
-      console.log(arr[1].nativeElement.style);
+      // console.log(arr[1].nativeElement.style);
       arr[0].nativeElement.style.height="25px";
       arr[0].nativeElement.style.width="25px";
       arr[1].nativeElement.style.height="15px";
@@ -50,7 +78,7 @@ export class HomeComponent implements OnInit {
       arr[2].nativeElement.style.width="15px";
     }
     else if(window.pageYOffset > 1000){
-      console.log("true");
+      // console.log("true");
       arr[0].nativeElement.style.height="15px";
       arr[0].nativeElement.style.width="15px";
       arr[1].nativeElement.style.height="15px";
